37c36105d403cc903bc66190858785b6d5f00ffe
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-07-01 13:58:16 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/merchants', to: 'merchants#index'
-  get '/merchants/new', to: 'merchants#new'
-  get '/merchants/:id', to: 'merchants#show'
-  post '/merchants', to: 'merchants#create'
-  get '/merchants/:id/edit', to: 'merchants#edit'
-  patch '/merchants/:id', to: 'merchants#update'
-  delete '/merchants/:id', to: 'merchants#destroy'
-
-  get '/items', to: 'items#index'
-  get '/merchants/:merchant_id/items', to: 'items#index'
-  get '/items/:id', to: 'items#show'
-  get '/merchants/:merchant_id/items/new', to: 'items#new'
-  post '/merchants/:merchant_id/items', to: 'items#create'
-  get '/items/:id/edit', to: 'items#edit'
-  patch '/items/:id', to: 'items#update'
-  delete '/items/:id', to: 'items#destroy'
+  resources :merchants do
+    resources :items, only: [:index, :new, :create]
+  end
+  resources :items, only: [:index, :show, :edit, :update, :destroy]
 end
