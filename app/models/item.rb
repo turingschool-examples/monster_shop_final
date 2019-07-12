e1212ead9aa1a93faeac1b013b3629d351436852
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-07-12 14:19:40 -0600

    User Story 25, Deleting Items Deletes its Reviews

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :order_items
   has_many :orders, through: :order_items
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   def sorted_reviews(limit = nil, order = :asc)
     reviews.order(rating: order).limit(limit)
--- spec/features/items/delete_spec.rb
+++ spec/features/items/delete_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe 'Delete Item' do
         @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
         @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
         @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+        @giant.reviews.create!(title: 'Great', description: 'Super duper', rating: 5)
       end
-      
+
       it 'I can click a link to delete that item' do
         visit "/items/#{@giant.id}"
 
@@ -21,6 +22,14 @@ RSpec.describe 'Delete Item' do
         expect(page).to have_css("#item-#{@ogre.id}")
         expect(page).to have_css("#item-#{@hippo.id}")
         expect(page).to_not have_css("#item-#{@giant.id}")
+      end
+
+      it 'Deleting an item, deletes its reviews as well' do
+        visit "/items/#{@giant.id}"
+
+        click_link 'Delete'
+
+        expect(Review.count).to eq(0)
       end
     end
   end
