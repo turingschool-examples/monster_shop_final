e5047dc385522f7b768a79c2bf6ef967b32f18ab
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-07-18 17:23:39 -0600

    User Story 16, User can log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,9 @@ class SessionsController < ApplicationController
   end
 
   def logout
+    session.delete(:user_id)
+    session.delete(:cart)
+    flash[:notice] = 'You have been logged out!'
     redirect_to root_path
   end
 
--- spec/features/users/login_logout_spec.rb
+++ spec/features/users/login_logout_spec.rb
@@ -109,4 +109,45 @@ RSpec.describe 'User Login and Log Out' do
       expect(page).to have_button('Log In')
     end
   end
+
+  describe 'A logged in user can log out' do
+    before :each do
+      @user = User.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+    end
+
+    it 'I visit the log out path' do
+      visit login_path
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log In'
+
+      click_link 'Log Out'
+
+      expect(current_path).to eq(root_path)
+      expect(page).to_not have_content("Logged in as #{@user.name}")
+      expect(page).to have_content('You have been logged out!')
+    end
+
+    it 'I log out and my cart is cleared' do
+      megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      ogre = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+
+      visit login_path
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log In'
+
+      visit item_path(ogre)
+
+      click_button 'Add to Cart'
+
+      expect(page).to have_link('Cart: 1')
+
+      click_link 'Log Out'
+
+      expect(page).to have_link('Cart: 0')
+    end
+  end
 end
