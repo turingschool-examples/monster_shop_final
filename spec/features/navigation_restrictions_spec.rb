195138b445366efd58dc8cb07e4e78963ee2bd7e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-07-18 10:51:49 -0600

    User Story 8, Merchant Navigation Restrictions

--- spec/features/navigation_restrictions_spec.rb
+++ spec/features/navigation_restrictions_spec.rb
@@ -37,4 +37,20 @@ RSpec.describe 'Navigation Restrictions' do
       expect(page.status_code).to eq(404)
     end
   end
+
+  describe 'As a Merchant User (Employee)' do
+    before :each do
+      @merchant = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @m_user = @merchant.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+    end
+
+    it 'I can not visit the admin dashboard' do
+      visit '/admin'
+    end
+
+    after :each do
+      expect(page.status_code).to eq(404)
+    end
+  end
 end
