058e43ea949fa4a33b7fb0dbbbe040c7f789ec74
Oscar Bellechasse <obellech10@gmail.com>
2019-07-28 15:03:57 -0600

    Add validations for name,email,password,address,city,state,zip. Also added attr accessor for user

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,16 +8,26 @@ class Registration
                 :zip,
                 :email,
                 :password,
-                :password_confirmation
+                :password_confirmation,
+                :user
+
+  validates_presence_of :name,
+                        :email,
+                        :password,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
 
   def save
     return false if invalid?
 
     ActiveRecord::Base.transaction do
-      user = User.create!(name: name, email: email, password: password)
-      user.address.create!(address: address, city: city, state: state, zip: zip)
+      @user = User.create(name: name, email: email, password: password)
+      errors.add(:email, @user.errors.messages[:email].first)
+      if @user.valid?
+        @user.addresses.create(address: address, city: city, state: state, zip: zip)
+      end
     end
-
-    true
   end
 end
