b87f6bf660ed9b173528ef36dbe51dc094c6dc4f
Oscar Bellechasse <obellech10@gmail.com>
2019-07-29 12:04:20 -0600

    Add methods for edit and update to the controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -13,6 +13,20 @@ class AddressesController < ApplicationController
     end
   end
 
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to profile_path
+    else
+      generate_flash(@address)
+      render :edit
+    end
+  end
+
   private
 
   def address_params
