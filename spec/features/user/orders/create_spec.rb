02075a10a49c448c34e9f17e83f087d1692bf530
Oscar Bellechasse <obellech10@gmail.com>
2019-07-29 19:40:52 -0600

    Update feature test for creating orders, added test for selecting address when checking out

--- spec/features/user/orders/create_spec.rb
+++ spec/features/user/orders/create_spec.rb
@@ -11,10 +11,12 @@ RSpec.describe 'Create Order' do
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 
-      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword') 
+      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      @address = Address.create!(nickname: 'Home', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, user_id: @user.id)
+      @address_1 = Address.create!(nickname: 'Home', address: '1776 Independence Blvd', city: 'Boston', state: 'MA', zip: 80218, user_id: @user.id)
+      @address_2 = Address.create!(nickname: 'Work', address: '1790 Democracy Ln', city: 'Washington', state: 'DC', zip: 80218, user_id: @user.id)
+      @address_3 = Address.create!(nickname: 'Vacation', address: '1969 Eagle Rd', city: 'Houston', state: 'TX', zip: 80218, user_id: @user.id)
     end
 
     it 'I can click a link to get to create an order' do
@@ -28,6 +30,13 @@ RSpec.describe 'Create Order' do
       visit '/cart'
 
       click_button 'Check Out'
+
+      expect(current_path).to eq(select_address_path)
+
+      within "#address-#{@address_3.id}" do
+        expect(page).to have_content('Vacation')
+        click_button 'Use this address'
+      end
 
       order = Order.last
 
