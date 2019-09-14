543ea43289521cc8524751e56fb9b9cbc462b7d6
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-14 17:35:45 -0600

    Update Order Model to include belongs_to optional for addresses.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   belongs_to :user
+  belongs_to :address, optional: true
 
   enum status: ['pending', 'packaged', 'shipped', 'cancelled']
 
