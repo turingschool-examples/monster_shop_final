f041ae817c3a3a18da101f6378a23827f9bf9002
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-14 20:33:09 -0600

    Create namespaced routes for user/addresses

--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,23 @@ Rails.application.routes.draw do
 
   get '/registration', to: 'users#new', as: :registration
   resources :users, only: [:create, :update]
+
   patch '/user/:id', to: 'users#update'
   get '/profile', to: 'users#show'
   get '/profile/edit', to: 'users#edit'
   get '/profile/edit_password', to: 'users#edit_password'
+
   post '/orders', to: 'user/orders#create'
   get '/profile/orders', to: 'user/orders#index'
   get '/profile/orders/:id', to: 'user/orders#show'
   delete '/profile/orders/:id', to: 'user/orders#cancel'
+
+  get '/profile/addresses/:id', to: 'user/addresses#new'
+  post '/profile/addresses/:id', to: 'user/addresses#create'
+  get '/profile/addresses/:id', to: 'user/addresses#edit'
+  get '/profile/addresses/:id', to: 'user/addresses#show'
+  patch '/profile/addresses/:id', to: 'user/addresses#update'
+  delete '/profile/addresses/:id', to: 'user/addresses#destroy'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#login'
