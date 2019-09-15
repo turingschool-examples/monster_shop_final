90b96782a0616e3afcde12dec1c33edbeef1b077
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-14 20:49:43 -0600

    Edit test for destroy method

--- spec/features/address/two_spec.rb
+++ spec/features/address/two_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Full Address CRUD" do
       click_on "Edit This Address"
 
       expect(current_path).to eq(edit_user_address_path(@user_address))
-
+      expect(page).to have_content("Edit Address Form")
     end
     it "Users can Create an address from their profile page" do
       visit profile_path
@@ -23,6 +23,7 @@ RSpec.describe "Full Address CRUD" do
       click_on "Create A New Address"
 
       expect(current_path).to eq(new_user_address_path(@user))
+      expect(page).to have_content("New Address Form")
 
     end
     it "Users can Delete an address from their profile page" do
@@ -31,8 +32,11 @@ RSpec.describe "Full Address CRUD" do
       expect(page).to have_link("Delete This Address")
       click_on "Delete This Address"
 
-      expect(current_path).to eq(delete_address_path(@user_address))
-
+      expect(current_path).to eq(profile_path)
+      expect(page).to_not have_content(@user_address.address)
+      expect(page).to_not have_content(@user_address.city)
+      expect(page).to_not have_content(@user_address.state)
+      expect(page).to_not have_content(@user_address.zip)
     end
   end
 end
