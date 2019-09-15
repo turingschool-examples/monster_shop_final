998a2bcd80dbc11c02a8f8e9d1a4783fec677a85
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-14 20:43:36 -0600

    Update test

--- spec/features/address/two_spec.rb
+++ spec/features/address/two_spec.rb
@@ -11,16 +11,28 @@ RSpec.describe "Full Address CRUD" do
       visit profile_path
 
       expect(page).to have_link("Edit This Address")
+      click_on "Edit This Address"
+
+      expect(current_path).to eq(edit_user_address_path(@user_address))
+
     end
     it "Users can Create an address from their profile page" do
       visit profile_path
 
       expect(page).to have_link("Create A New Address")
+      click_on "Create A New Address"
+
+      expect(current_path).to eq(new_user_address_path(@user))
+
     end
     it "Users can Delete an address from their profile page" do
       visit profile_path
 
       expect(page).to have_link("Delete This Address")
+      click_on "Delete This Address"
+
+      expect(current_path).to eq(delete_address_path(@user_address))
+
     end
   end
 end
