9de2561291ce61d3582b3ffe20887935724c80cd
Kelly Sandeson <kellysandeson@gmail.com>
2019-09-14 22:44:15 -0600

    Update controller methods to use current user

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -1,12 +1,12 @@
 class User::AddressesController < User::BaseController
 
   def new
-    @user = User.find(params[:format])
+    @user = current_user
     @address = @user.addresses.build
   end
 
   def create
-    @user = User.find(params[:id])
+    @user = current_user
     @address = @user.addresses.create(address_params)
     redirect_to profile_path
   end
