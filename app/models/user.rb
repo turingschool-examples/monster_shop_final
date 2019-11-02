c52b5601b61b75e099f2b36db1a8bb7b9512e4be
Jon Patterson <JonPatt92@gmail.com>
2019-11-01 22:12:35 -0600

    Add instance methods to verify unique nicknames in address table, call address object associated with default_address column, and assign a default address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,27 @@ class User < ApplicationRecord
   has_many :addresses
 
   validates_presence_of :name,
-                        :address,
                         :email
 
   validates_uniqueness_of :email
 
   enum role: ['default', 'merchant_admin', 'admin']
+
+    # Write Tests!
+    # Find way to make private without disabling assign_address being called in address model
+
+    #Verify this can go here.
+    # Called in controllers/user/addresses_controller.rb Create method
+    # Returns true if none of THIS User's Addresses' nicknames include the nickname (passed as an argument) that will be used to create the new Address.
+    def nickname_uniq?(nickname)
+      !addresses.pluck(:nickname).include?(nickname)
+    end
+
+    def my_address
+      addresses.find(self.default_address) if default_address != nil
+    end
+
+    def assign_address(address_id)
+      self.default_address = address_id
+    end
 end
