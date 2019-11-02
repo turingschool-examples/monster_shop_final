dcbd175b735ef51d5eed9dac694c804b44fa035f
Christopher Kelling <cj.kelling88@gmail.com>
2019-11-02 12:17:44 -0600

    Create test to verify a merchant can edit their coupons

--- spec/features/merchant/coupons/crud_funtionality_spec.rb
+++ spec/features/merchant/coupons/crud_funtionality_spec.rb
@@ -6,20 +6,21 @@ describe 'As a Merchant' do
   describe 'I have full CRUD functionality over my coupons' do
     before :each do
       merchant = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80_218)
-      user = merchant.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80_218, email: 'meganexample.com', password: 'securepassword')
       @coupon = merchant.coupons.create!(name: 'New User Discount', discount: 10)
+
+      user = merchant.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80_218, email: 'meganexample.com', password: 'securepassword')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit merchant_coupons_path
     end
 
     it 'I can create a coupon' do
-      save_and_open_page
       click_link 'Create New Coupon'
 
-      expect(current_path).to eq(merchant_coupons_new_path)
+      expect(current_path).to eq(new_merchant_coupon_path)
 
       fill_in 'Name', with: 'Bulk Discount'
-      fill_in 'Discount', with: '10%'
+      fill_in 'Discount', with: 10
       click_button 'Create Coupon'
 
       expect(current_path).to eq(merchant_coupons_path)
@@ -33,6 +34,28 @@ describe 'As a Merchant' do
     end
 
     it 'I can edit a coupon' do
+      within "#coupon-#{@coupon.id}" do
+        click_link 'Edit Coupon'
+      end
+
+      expect(current_path).to eq(edit_merchant_coupon_path(@coupon))
+
+      expect(find_field(:name).value).to eq(@coupon.name)
+      expect(find_field(:discount).value).to eq(@coupon.discount.to_s)
+
+      fill_in 'name', with: 'New Discount'
+      fill_in 'discount', with: 15
+      click_button 'Update Coupon'
+
+      expect(current_path).to eq(merchant_coupons_path)
+
+      @coupon.reload
+      visit merchant_coupons_path
+
+      within "#coupon-#{@coupon.id}" do
+        expect(page).to have_content('Coupon Name: New Discount')
+        expect(page).to have_content('Discount: 15%')
+      end
     end
 
     it 'I can enable/disable a coupon' do
