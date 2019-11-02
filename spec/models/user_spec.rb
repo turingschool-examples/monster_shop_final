d09be4443c5694d7349e6103891ee5947e14b051
Jon Patterson <JonPatt92@gmail.com>
2019-11-01 22:13:05 -0600

    Remove now invalid validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User do
 
   describe 'Validations' do
     it {should validate_presence_of :name}
-    it {should validate_presence_of :address}
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
