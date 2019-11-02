f427f08fcfbe20e68126cde3d2f808b1f1a6c46b
Jonathan Patterson <jonpatt92@gmail.com>
2019-11-02 00:54:42 -0600

    Add notes for existing methods and ongoing issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,17 +15,20 @@ class User < ApplicationRecord
     # Write Tests!
     # Find way to make private without disabling assign_address being called in address model
 
-    #Verify this can go here.
-    # Called in controllers/user/addresses_controller.rb Create method
-    # Returns true if none of THIS User's Addresses' nicknames include the nickname (passed as an argument) that will be used to create the new Address.
+      #Verify this can go here.
+      # Called in controllers/user/addresses_controller.rb Create method
+      # Returns true if none of THIS User's Addresses' nicknames include the nickname (passed as an argument) that will be used to create the new Address.
     def nickname_uniq?(nickname)
       !addresses.pluck(:nickname).include?(nickname)
     end
 
+      # Searches child Addresses for the Address who's :id matches the User's default_address:address.id  
     def my_address
       addresses.find(self.default_address) if default_address != nil
     end
 
+      # Changes the current User's default_address column = to the address_id passed as an argument. Problem: Should be private yet *still accessible from the address model* **or find a better way to assign the default :id
+      # Whenever the number of User Addresses == 1, either on creation of the 1st address. Or deletion of the 2nd existing address. 
     def assign_address(address_id)
       self.default_address = address_id
     end
