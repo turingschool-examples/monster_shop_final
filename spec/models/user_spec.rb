7df8fb49d08f9ad47fe48919feef57b30e16708e
Jon Patterson <JonPatt92@gmail.com>
2019-11-04 15:16:46 -0700

    Build instance method tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,29 @@ RSpec.describe User do
     it {should validate_presence_of :email}
     it {should validate_uniqueness_of :email}
   end
+
+  describe 'Instance Methods' do
+    before :each do
+      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      @admin = User.create!(name: 'Megan', email: 'admin@example.com', password: 'securepassword', role: 2)
+      @address_1 = @user.addresses.create!(address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @address_2 = @user.addresses.create!(address: '456 Main st', city: 'Dallas', state: 'TX', zip: 75402, nickname: 'Work')
+    end
+
+    it '.my_address' do
+      expect(@user.my_address).to eq(@address_1)
+    end
+
+    it '.assign_address' do
+      expect(@user.default_address).to eq(@address_1.id)
+      @user.assign_address(@address_2.id)
+      expect(@user.default_address).to eq(@address_2.id)
+    end
+
+    it '.nickname_uniq?' do
+      expect(@user.nickname_uniq?('Home')).to eq(false)
+      expect(@user.nickname_uniq?('Work')).to eq(false)
+      expect(@user.nickname_uniq?('gf')).to eq(true)
+    end
+  end
 end
