5735e03a3910c82b92ee2743acd214d492dc1706
Phil DeLong <philjdelong@gmail.com>
2020-01-13 17:23:51 -0700

    Add new and create to coupons controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -1,6 +1,27 @@
 class Merchant::CouponsController < Merchant::BaseController
 
   def index
+    @merchant = current_user.merchant
+  end
 
+  def new
+  end
+
+  def create
+    merchant = current_user.merchant
+    coupon = merchant.coupons.create(coupon_params)
+    if coupon.save
+      flash[:success] = "Coupon Created!"
+      redirect_to '/merchant/coupons'
+    else
+      flash[:error] = "Please enter valid coupon info."
+      redirect_to '/merchant/coupons/new'
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :code, :percentage)
   end
 end
