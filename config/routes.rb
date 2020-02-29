584ca8aa992a8d0a40181f4091abdd19ba70b7e6
Ben Fox <benfox1216@gmail.com>
2020-02-28 23:11:43 -0700

    Add / route so heroku works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :root, to: 'welcome#index'
+  get '/', to: 'welcome#index'
 
   resources :merchants do
     resources :items, only: [:index]
