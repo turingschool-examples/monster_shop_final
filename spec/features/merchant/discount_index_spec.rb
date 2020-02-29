45d2a6737302ff371c6e912bcdf9d5a7023f47cd
KellyIB <kellyibard@gmail.com>
2020-02-29 09:25:42 -0700

    add test file and tests for dicsount show page

--- spec/features/merchant/discount_index_spec.rb
+++ spec/features/merchant/discount_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Discount Dashboard' do
+RSpec.describe 'Discount Index Page' do
   describe 'As an employee of a merchant' do
     before :each do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
