4e5c26a8e01726e90bc5a8e75da8a83647115ebd
KellyIB <kellyibard@gmail.com>
2020-02-29 22:18:51 -0700

    add relationships to models

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,11 +1,21 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :order_items, through: :items
+  has_many :orders, through: :order_items
 
   validates_presence_of :percent_off,
                         :quantity_threshold,
                         :status
 
-
   enum status: ['active', 'inactive']
+
+  def has_not_been_used(id)
+    discounted_order_items = OrderItem.where(discount_id: id)
+    discounted_order_items == []
+  end
+
+
+
 
 end
