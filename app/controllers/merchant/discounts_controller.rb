635c5ea88c93a15366bbba8def2b92c386c03358
Pierce Alworth <pbalworth@gmail.com>
2020-03-02 22:37:32 -0700

    update discount controller and add new and update method

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -11,8 +11,8 @@ class Merchant::DiscountsController < Merchant::BaseController
   end
 
   def create
-	@merchant = current_user.merchant
-	@discount = @merchant.discounts.new(permitted_discount_params)
+  	@merchant = current_user.merchant
+  	@discount = @merchant.discounts.new(private_discount_params)
   	if @discount.save
   		flash[:notice] = 'You just made a new discount!'
   		redirect_to '/merchant/discounts'
@@ -22,9 +22,24 @@ class Merchant::DiscountsController < Merchant::BaseController
    end
  end
 
+ def edit
+   @discount = Discount.find(params[:discount_id])
+ end
+
+ def update
+   discount = Discount.find(params[:discount_id])
+    if discount.update(private_discount_params)
+      flash[:notice] = "Your discount changes have been saved."
+      redirect_to "/merchant/discounts/#{discount.id}"
+    else
+      flash[:notice] = "Your discount changes have not been saved."
+      redirect_to "/merchant/discounts/#{discount.id}/edit"
+    end
+  end
+
  private
- 
- def permitted_discount_params
+
+ def private_discount_params
     params.permit(:title, :information, :lowest_amount, :highest_amount, :percent_off)
  end
 
