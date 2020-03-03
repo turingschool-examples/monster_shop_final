5cfaae7459be949b17fc9f073a2f4738df658563
Pierce Alworth <pbalworth@gmail.com>
2020-03-02 21:29:47 -0700

    Add new, create, and private params

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,26 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:discount_id])
   end
+
+  def new
+  end
+
+  def create
+	@merchant = current_user.merchant
+	@discount = @merchant.discounts.new(permitted_discount_params)
+  	if @discount.save
+  		flash[:notice] = 'You just made a new discount!'
+  		redirect_to '/merchant/discounts'
+  	else
+  		flash[:notice] = 'Failed to create new discount, try again!'
+  		redirect_to '/merchant/discounts/new'
+   end
+ end
+
+ private
+ 
+ def permitted_discount_params
+    params.permit(:title, :information, :lowest_amount, :highest_amount, :percent_off)
+ end
+
 end
