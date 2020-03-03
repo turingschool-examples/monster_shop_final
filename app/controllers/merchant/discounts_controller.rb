b32addb8276bce28ad4db4e54164826f022586fa
Pierce Alworth <pbalworth@gmail.com>
2020-03-02 20:13:10 -0700

    Add show method for show

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,8 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @discounts = Discount.all
   end
+
+  def show
+    @discount = Discount.find(params[:discount_id])
+  end
 end
