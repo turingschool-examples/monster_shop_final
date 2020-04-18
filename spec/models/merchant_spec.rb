f25c20fc8aa5db104ab908e4af063859e43c5960
Steven Meyers <sjjmeyers@gmail.com>
2020-04-18 16:35:50 -0600

    Add discount relation to Merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Merchant do
   describe 'Relationships' do
     it {should have_many :items}
+    it {should have_many :discounts}
     it {should have_many(:order_items).through(:items)}
     it {should have_many :users}
   end
