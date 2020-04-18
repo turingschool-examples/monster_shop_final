277e8f99346d913378d2431c0c081ee57423145b
Alex Latham <alexlthm@gmail.com>
2020-04-18 14:10:41 -0600

    Fix factories

--- spec/factories/order_item.rb
+++ spec/factories/order_item.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :item_order, class: ItemOrder do
+  factory :order_item, class: OrderItem do
     price        { Faker::Commerce.price }
     quantity     { Faker::Number.between(from: 1, to: 200) }
     association  :item, factory: :item
