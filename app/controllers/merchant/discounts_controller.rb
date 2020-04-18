7aa683daedacc71f6bc3389eee33f4020b5d0f71
Alex Latham <alexlthm@gmail.com>
2020-04-18 16:07:45 -0600

    Add controller action and view for new discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,24 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @discounts = Discount.where(merchant: current_user.merchant)
   end
+
+  def new
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path
+    else
+      generate_flash(item)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percent_off, :minimum_quantity)
+  end
 end
