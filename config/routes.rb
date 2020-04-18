3b105d186ffe539f14074f2f64cd5c821918c7f1
Alex Latham <alexlthm@gmail.com>
2020-04-18 15:12:43 -0600

    Add route for /merchant/discounts

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
+    resources :discounts, only: [:index]
   end
 
   namespace :admin do
