82966488bf78a09c01730fde90380b47edf3c6ba
Alex Latham <alexlthm@gmail.com>
2020-04-18 12:39:05 -0600

    Replace root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get :root, to: 'welcome#index'
+  root 'welcome#index'
 
   resources :merchants do
     resources :items, only: [:index]
