9418f9c858ecea55a075101290f428f29788079d
Alex Latham <alexlthm@gmail.com>
2020-04-18 14:04:17 -0600

    Add tests for appearance of links to My Items and My Discounts

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -62,5 +62,17 @@ RSpec.describe 'Merchant Dashboard' do
 
       expect(current_path).to eq("/merchant/orders/#{@order_2.id}")
     end
+
+    it "I can link to my items" do
+      visit '/merchant'
+
+      expect(page).to have_link("My Items", href: "/merchant/items")
+    end
+
+    it "I can link to my bulk discounts" do
+      visit '/merchant'
+
+      expect(page).to have_link("My Discounts", href: "/merchant/discounts")
+    end
   end
 end
