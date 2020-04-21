e3121ec83fa52c4bffcb83ba33839fb647308971
Ethan Hocking <eshocking253@gmail.com>
2020-04-21 17:02:42 -0600

    Add create action to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -8,4 +8,25 @@ class Merchant::DiscountsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to "/merchant/discounts"
+    else
+      generate_flash(discount)
+      render :new
+    end
+  end
+
+
+  private
+
+  def discount_params
+    if params[:percent]
+      params[:percent] = params[:percent].to_f / 100
+    end
+    params.permit(:name, :percent, :threshold)
+  end
 end
