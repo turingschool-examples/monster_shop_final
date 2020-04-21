46c5b57327a6b738d9368e37ab1bd0a6ecc6f2fd
Steven Meyers <sjjmeyers@gmail.com>
2020-04-21 11:13:40 -0600

    Git add calculation_percentage discount model method

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe Discount do
           .with_message("Percentage must be 1 - 99")}
     it { should validate_numericality_of(:quantity).is_greater_than(0) }
   end
+
+  describe 'instance methods' do
+    it '.calculation_percentage' do
+      brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      discount_1 = Discount.create!(quantity: 5, percentage: 10, merchant_id: brian.id)
+      expect(discount_1.calculation_percentage).to eq(0.9)
+    end
+  end
 end
