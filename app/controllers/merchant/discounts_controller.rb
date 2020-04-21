6e8b20b20f152a652321246e980c57b2f2811876
Ethan Hocking <eshocking253@gmail.com>
2020-04-21 16:05:35 -0600

    Add show action to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,8 @@ class Merchant::DiscountsController < ApplicationController
   def index
     @discounts = Discount.where(merchant_id: current_user.merchant.id)
   end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
 end
