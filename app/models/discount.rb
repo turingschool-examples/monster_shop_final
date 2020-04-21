1e83629b3e9c196a4c34390461a7e897161f3f01
Steven Meyers <sjjmeyers@gmail.com>
2020-04-21 14:57:43 -0600

    Remove discount model method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,7 +4,4 @@ class Discount < ApplicationRecord
   validates_inclusion_of :percentage, in: 1..99, message: "Percentage must be 1 - 99"
   validates_numericality_of :quantity, greater_than: 0
 
-  def calculation_percentage
-    (100 - percentage) * 0.01
-  end
 end
