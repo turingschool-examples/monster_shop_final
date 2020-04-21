b690cb0cff7c3a71533ec3b7459dae3ebdb0f12f
Ethan Hocking <eshocking253@gmail.com>
2020-04-21 16:03:59 -0600

    Add route to discount show pages

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
-    resources :discounts, only: [:index]
+    resources :discounts, only: [:index, :show]
   end
 
   namespace :admin do
