0d8b6bd4b5068b7266ed9de56545e78a31f89d04
Alex Latham <alexlthm@gmail.com>
2020-04-27 20:55:25 -0600

    Add profile orders controller

---
+++ app/controllers/profile/orders_controller.rb
@@ -0,0 +1,30 @@
+class  Profile::OrdersController < Profile::BaseController
+  def index
+    @orders = current_user.orders
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def create
+    order = current_user.orders.new
+    order.save
+      cart.items.each do |item|
+        order.order_items.create({
+          item: item,
+          quantity: cart.count_of(item.id),
+          price: item.price * (1 - cart.discount_rate(item))
+          })
+      end
+    session.delete(:cart)
+    flash[:notice] = "Order created successfully!"
+    redirect_to '/profile/orders'
+  end
+
+  def destroy
+    order = current_user.orders.find(params[:id])
+    order.cancel
+    redirect_to "/profile/orders/#{order.id}"
+  end
+end
