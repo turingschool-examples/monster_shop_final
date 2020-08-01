e3559b74e4fbb2ec0396557c19885b3fc37e6b78
Eric Hale <eric_hale@comcast.net>
2020-08-01 16:54:20 -0600

    Add test for merchant discounts path from dashboard

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -62,5 +62,13 @@ RSpec.describe 'Merchant Dashboard' do
 
       expect(current_path).to eq("/merchant/orders/#{@order_2.id}")
     end
+
+    it "I can see a link to discounts" do
+      visit '/merchant'
+
+      click_link "Discounts"
+
+      expect(current_path).to eq("/merchant/discounts")
+    end
   end
 end
