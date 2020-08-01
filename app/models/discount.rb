8f3d830d5321b9aab08f6624c16149edcf713936
“Neeru <neeram85@gmail.com>
2020-08-01 12:35:34 -0600

    Added validations to discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,8 @@
 class Discount < ApplicationRecord
   has_many :merchant_discounts
   has_many :merchants, through: :merchant_discounts
+
+  validates_presence_of :percent,
+                        :req_inventory
+
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Discount do
+  describe 'Validations' do
+    it {should validate_presence_of :percent}
+    it {should validate_presence_of :req_inventory}
+  end
+
   describe 'Relationships' do
     it {should have_many(:merchants).through(:merchant_discounts)}
     it {should have_many :merchant_discounts}
