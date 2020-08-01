9f204aa5eda1a7fb20426e3956b4e9364034dedf
Dan Sehl <dan.sehl@gmail.com>
2020-08-01 14:38:29 -0600

    Change seed file to have merchant employee made by a merchant

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,12 @@ User.destroy_all
 Review.destroy_all
 
 User.create!(name: 'Admin', address: '123 Who Cares Ln', city: 'Denver', state: 'CO', zip: '12345', email: 'admin@admin.com', password: 'admin', role: 2)
-User.create!(name: 'Merchant Employee', address: '456 Blah Blah Blvd', city: 'Denver', state: 'CO', zip: '12345', email: 'merchant@merchant.com', password: 'merchant', role: 1)
 User.create!(name: 'Default', address: '789 Hello St', city: 'Denver', state: 'CO', zip: '12345', email: 'default@default.com', password: 'default', role: 0)
 
 megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+megan.users.create!(name: 'Merchant Employee', address: '456 Blah Blah Blvd', city: 'Denver', state: 'CO', zip: '12345', email: 'merchant@merchant.com', password: 'merchant')
+
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
