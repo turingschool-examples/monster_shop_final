8fc5fba121dcc7f3f3c9bb6cf27b4cbd312f120f
Dan Sehl <dan.sehl@gmail.com>
2020-08-01 14:38:03 -0600

    Test for US1 discounts index

---
+++ spec/features/discount/index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discount Index' do
+  describe 'As an employee of a merchant' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @m_user = @merchant_1.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      @discount_1 = @merchant_1.discounts.create!(name: "5% off 20 or more items", min_item_quantity: 20, percent_off: 5)
+    end
+
+    it 'I can see my merchants information on the merchant dashboard' do
+      visit '/merchant'
+
+      click_link "View All Bulk Discounts"
+      expect(current_path).to eq("/merchant/discounts")
+
+      expect(page).to have_link(@discount_1.name)
+      expect(page).to have_content(@discount_1.min_item_quantity)
+      expect(page).to have_content(@discount_1.percent_off)
+    end
+  end
+end
