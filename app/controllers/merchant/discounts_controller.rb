0bb85553905535e64c4e8307d248190ab99195b1
“Neeru <neeram85@gmail.com>
2020-08-02 08:57:10 -0600

    Discount index page displays all merchant discounts

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,5 +1,6 @@
 class Merchant::DiscountsController < Merchant::BaseController
   def index
-
+    merchant = Merchant.find(current_user.merchant_id)
+    @discounts = merchant.discounts
   end
 end
---
+++ spec/features/merchant/discounts/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discount Index Page' do
+  describe 'As an employee of a merchant' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @discount_1 = @merchant_1.discounts.create(percent: 5, quantity: 10)
+      @discount_2 = @merchant_1.discounts.create(percent: 10, quantity: 20)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+    end
+
+    it "I see all of my bulk discounts listed with percentage and inventory quantity" do
+      visit '/merchant/discounts'
+
+      expect(page).to have_content("Bulk Discounts")
+
+
+      within ".discount-#{@discount_1.id}" do
+        expect(page).to have_content("Percent Off: #{@discount_1.percent}")
+        expect(page).to have_content("Required Item Quantity: #{@discount_1.quantity} units")
+      end
+
+      within ".discount-#{@discount_2.id}" do
+        expect(page).to have_content("Percent Off: #{@discount_2.percent}")
+        expect(page).to have_content("Required Item Quantity: #{@discount_2.quantity} units")
+      end
+    end
+  end
+end
