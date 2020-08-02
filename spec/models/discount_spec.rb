2b9f1718f5cc4d6e16f23cb0f85f840b52b6c343
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-01 21:01:41 -0400

    Add discount model spec

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Discount do
   describe 'Relationships' do
     it {should belong_to :merchant}
-    it {should have_many :orders}
   end
 
   describe 'Validations' do
     it {should validate_presence_of :percentage}
+    it {should validate_presence_of :required_amount}
   end
+end
