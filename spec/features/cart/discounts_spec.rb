a9d3c0aadc2db4306b10ecb9125d6bc07148d819
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-02 09:14:58 -0400

    Add test that discount will apply only to items the belong to the given merchant

--- spec/features/cart/discounts_spec.rb
+++ spec/features/cart/discounts_spec.rb
@@ -8,9 +8,10 @@ RSpec.describe 'Merchant Discount New Page' do
       @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.00, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 1 )
+      @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @naf = User.create!(name: 'Naftali Schaechter',  address: '190 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'nafnaf@naf.naf', password: 'iamnaf')
       @discount_1 = @merchant_1.discounts.create!(percentage: 10, required_amount: 5)
+      @discount_2 = @merchant_2.discounts.create!(percentage: 20, required_amount: 2)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@naf)
     end
 
@@ -67,6 +68,41 @@ RSpec.describe 'Merchant Discount New Page' do
       end
 
       within "#item-#{@ogre.id}" do
+        expect(page).to have_content("Subtotal: $80.00")
+      end
+    end
+
+    it 'A discount will only be applied to items that belong to the given merchant' do
+      visit "/items/#{@ogre.id}"
+
+      click_button 'Add to Cart'
+
+      visit "/items/#{@hippo.id}"
+
+      click_button 'Add to Cart'
+
+      visit '/cart'
+
+      within "#item-#{@ogre.id}" do
+        expect(page).to have_content("Subtotal: $20.00")
+
+        click_button 'More of This!'
+        click_button 'More of This!'
+        click_button 'More of This!'
+
+        expect(page).to have_content("Subtotal: $80.00")
+
+        click_button 'More of This!'
+
+        expect(page).to have_content("Subtotal: $90.00")
+        expect(page).to_not have_content("Subtotal: $100.00")
+      end
+
+      within "#item-#{@hippo.id}" do
+        expect(page).to have_content("Subtotal: $50.00")
+
+        click_button 'More of This!'
+
         expect(page).to have_content("Subtotal: $80.00")
       end
     end
