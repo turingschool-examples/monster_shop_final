1f375c6b2be576823415c95c63ea9903a12bdf7f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-01 21:42:24 -0400

    Add bulk discounts new page spec

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,4 +1,5 @@
 class Merchant::DiscountsController < Merchant::BaseController
   def index
+    @merchant = current_user.merchant
   end
 end
--- spec/features/merchant/discounts_new_spec.rb
+++ spec/features/merchant/discounts_new_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Merchant Discount Index Page' do
+RSpec.describe 'Merchant Discount New Page' do
   describe 'As a Merchant' do
     before :each do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
@@ -18,19 +18,13 @@ RSpec.describe 'Merchant Discount Index Page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
     end
 
-    it 'I can access my bulk discounts page via the dashboard' do
-      visit '/merchant'
-
-      expect(page).to have_link("My Bulk Discounts")
-      click_link "My Bulk Discounts"
-
-      expect(current_path).to eq("/merchant/discounts")
-    end
-
-    it 'I see a link to create a new discount' do
+    it 'I can access my bulk discounts new page via the bulk discounts index page' do
       visit '/merchant/discounts'
 
       expect(page).to have_link("Create New Discount")
+      click_link "Create New Discount"
+
+      expect(current_path).to eq("/merchant/discounts/new")
     end
   end
 end
