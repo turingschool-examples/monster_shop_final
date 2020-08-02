839119246f18914b753ca4bd3dfbe4ffac239577
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-01 23:05:39 -0400

    Add discount edit page

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
     get '/discounts', to: 'discounts#index'
     get '/discounts/new', to: 'discounts#new'
     post '/discounts', to: 'discounts#create'
+    get '/discounts/edit/:discount_id', to: 'discounts#edit'
   end
 
   namespace :admin do
