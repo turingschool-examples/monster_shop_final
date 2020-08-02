e18af34b6fe2c8bd2f444fa6a253ca6b530135d1
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-02 15:39:31 -0600

    Make Discount model test for relationships and validations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,9 +4,7 @@ class Discount < ApplicationRecord
                         :percent_off,
                         :min_quantity
 
-  validates  :percent_off
-  validates  :min_quantity
-
-
+  validates  :percent_off, numericality: { :only_integer => true, :less_than_or_equal_to => 100 }
+  validates  :min_quantity, numericality: { :only_integer => true, :greater_than => 0 }
 
 end
