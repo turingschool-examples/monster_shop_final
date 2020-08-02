73cf06b9d42331fa7de25b139567b91fee601c74
“Neeru <neeram85@gmail.com>
2020-08-02 10:11:05 -0600

    Validate numericality of percent and quantity for Discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,7 @@
 class Discount < ApplicationRecord
   validates_presence_of :percent
   validates_presence_of :quantity
+  validates_numericality_of :percent
+  validates_numericality_of :quantity  
   belongs_to :merchant
 end
