d549bbeb8a87477cf1811ca9090d801a40981817
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-01 22:12:46 -0400

    Add form for new bulk discount: {

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,5 +4,20 @@ class Merchant::DiscountsController < Merchant::BaseController
   end
 
   def new
+    @discount = Discount.new
+  end
+
+  def create
+    @discount = Discount.new(discount_params)
+    if @discount.save
+      redirect_to "/merchant/discounts"
+    else
+      render :new
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:percentage, :required_amount)
   end
 end
