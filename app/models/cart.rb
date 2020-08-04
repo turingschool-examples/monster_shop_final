8ce68e91611ecf1ec62fa4aa17ee1e5c6dcaa113
Eric Hale <eric_hale@comcast.net>
2020-08-04 15:26:53 -0600

    Add if block to subtotal to allow for discounts and change grand_total to be based off of subtotals

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -23,28 +23,26 @@ class Cart
       Item.find(item_id)
     end
   end
-#discount
+
   def grand_total
     grand_total = 0.0
     @contents.each do |item_id, quantity|
-      #change to be based off of sub total
       grand_total += subtotal_of(item_id)
     end
     grand_total
   end
-#discount
+
   def count_of(item_id)
     @contents[item_id.to_s]
   end
-# discount
+
   def subtotal_of(item_id)
     item = Item.find(item_id)
-    #require "pry"; binding.pry
     if discount_eligible?(item_id).nil?
       @contents[item_id.to_s] * Item.find(item_id).price
     else
-      # discount = discount_eligible?(item_id)
-      # @contents[item_id.to_s] * Item.find(item_id).price
+      discount = discount_eligible?(item_id).percentage.to_f / 100
+      @contents[item_id.to_s] * Item.find(item_id).price - discount
     end
   end
 
@@ -55,7 +53,7 @@ class Cart
   def discount_eligible?(item_id)
     item = Item.find(item_id)
     item_count = count_of(item_id)
-    item.discounts
-    item.discounts.where("item_amount <= #{item_count}").first
+    ordered_discounts = item.discounts.order(:item_amount)
+    ordered_discounts.where("item_amount <= #{item_count}").last
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,15 +7,22 @@ RSpec.describe Cart do
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @m_user = @megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       @discount1 = @m_user.merchant.discounts.create(percentage: 5, item_amount: 5, description: 'Five percent off of five items or more!')
-      @discount2 = @m_user.merchant.discounts.create(percentage: 15, item_amount: 10, description: 'Fifteen percent off of ten items or more!')
+      @discount2 = @m_user.merchant.discounts.create(percentage: 20, item_amount: 10, description: 'Fifteen percent off of ten items or more!')
+      @discount3 = @m_user.merchant.discounts.create(percentage: 15, item_amount: 9, description: 'Fifteen percent off of ten items or more!')
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 2 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @candle = @megan.items.create!(name: 'Candle', description: "I'll light up your life!", price: 5, image: 'https://cf.ltkcdn.net/candles/images/orig/257384-1600x1030-white-candle-magic-spells.jpg', active: true, inventory: 30 )
       @dish_towel = @megan.items.create!(name: 'Dish Towel', description: "I'll clean it up!", price: 10, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSKTzwz1piuGJO5B0aUlunlY9LCM0wVP1wkag&usqp=CAU', active: true, inventory: 100 )
+      @everyting = @brian.items.create!(name: 'Everything Bagel', description: "I've got anything you want!", price: 12, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQos5CbFANK1ZNGcqT-DycLcspuLqxqfRLRNE-HEYkqYKy88NOu8HjTeiGuiFyq5c7kxEIsmuX6&usqp=CAc', active: true, inventory: 100 )
       @cart = Cart.new({
         @ogre.id.to_s => 1,
         @giant.id.to_s => 2
+        })
+      @cart2 = Cart.new({
+        @ogre.id.to_s => 4,
+        @giant.id.to_s => 2,
+        @candle.id.to_s => 5
         })
     end
 
@@ -46,6 +53,7 @@ RSpec.describe Cart do
     #discount
     it '.grand_total' do
       expect(@cart.grand_total).to eq(120)
+      expect(@cart2.grand_total).to eq(204.95)
     end
     #discount
     it '.count_of()' do
@@ -54,14 +62,10 @@ RSpec.describe Cart do
     end
     #discount
     it '.subtotal_of()' do
-      cart = Cart.new({
-        @ogre.id.to_s => 4,
-        @giant.id.to_s => 2,
-        @candle.id.to_s => 5
-        })
+
       expect(@cart.subtotal_of(@ogre.id)).to eq(20)
       expect(@cart.subtotal_of(@giant.id)).to eq(100)
-      expect(cart.subtotal_of(@candle.id)).to eq(25)
+      expect(@cart2.subtotal_of(@candle.id)).to eq(24.95)
     end
 
     it '.limit_reached?()' do
@@ -76,17 +80,24 @@ RSpec.describe Cart do
     end
 
     it ".discount_eligible?()" do
-      cart = Cart.new({
-        @ogre.id.to_s => 4,
-        @giant.id.to_s => 2,
-        @candle.id.to_s => 5
-        })
-
-      expect(cart.discount_eligible?(@ogre.id)).to eq(nil)
-      expect(cart.discount_eligible?(@giant.id)).to eq(nil)
-      expect(cart.discount_eligible?(@candle.id)).to eq(@discount1)
-      cart.add_item(@ogre.id.to_s)
-      expect(cart.discount_eligible?(@ogre.id)).to eq(@discount1)
+      expect(@cart2.discount_eligible?(@ogre.id)).to eq(nil)
+      expect(@cart2.discount_eligible?(@giant.id)).to eq(nil)
+      expect(@cart2.discount_eligible?(@candle.id)).to eq(@discount1)
+      @cart2.add_item(@ogre.id.to_s)
+      expect(@cart2.discount_eligible?(@ogre.id)).to eq(@discount1)
+      @cart2.add_item(@candle.id.to_s)
+      @cart2.add_item(@candle.id.to_s)
+      @cart2.add_item(@candle.id.to_s)
+      @cart2.add_item(@candle.id.to_s)
+      expect(@cart2.discount_eligible?(@candle.id)).to eq(@discount3)
+      @cart2.add_item(@candle.id.to_s)
+      expect(@cart2.discount_eligible?(@candle.id)).to eq(@discount2)
+      @cart2.add_item(@everyting.id.to_s)
+      @cart2.add_item(@everyting.id.to_s)
+      @cart2.add_item(@everyting.id.to_s)
+      @cart2.add_item(@everyting.id.to_s)
+      @cart2.add_item(@everyting.id.to_s)
+      expect(@cart2.discount_eligible?(@everyting.id)).to eq(nil)
     end
   end
 end
