dccd7726411e7bf57e9d15426ff3b73bd7199a27
twarbelow <taija.warbelow@google.com>
2020-08-04 15:40:30 -0700

    add discounts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,5 @@ megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image:
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 m_user = megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
+discount_1 = merchant_1.discounts.create!(percent: 5, quantity_required: 10)
+discount_2 = merchant_1.discounts.create!(percent: 10, quantity_required: 15)
