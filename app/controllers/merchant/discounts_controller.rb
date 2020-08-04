8b3b647f6b02fa1f6b963ea069914e8814845717
twarbelow <taija.warbelow@google.com>
2020-08-04 11:38:08 -0700

    create action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,6 +4,20 @@ class Merchant::DiscountsController < Merchant::BaseController
   end
 
   def new
-    @discount = Discount.new
+    @discount = current_user.merchant.discounts.new
+  end
+
+  def create
+    merchant = params[:discount][:merchant_id]
+    percent = params[:discount][:percent]
+    quantity = params[:discount][:quantity_required]
+    if Discount.where("percent = ? and quantity_required = ? and merchant_id = ?", percent, quantity, merchant).empty?
+      Discount.create(percent: percent, quantity_required: quantity, merchant_id: merchant)
+      flash[:success] = "New discount created."
+      redirect_to "merchant/discounts"
+    else
+      flash[:error] = "This discount already exists for your shop."
+      redirect_to "merchant/discounts"
+    end
   end
 end
