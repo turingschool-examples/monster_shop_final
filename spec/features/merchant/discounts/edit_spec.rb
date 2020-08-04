18d006b24a4837e070094179b86fea2ba2c63fc6
twarbelow <taija.warbelow@google.com>
2020-08-04 13:55:34 -0700

    first third of edit spec

---
+++ spec/features/merchant/discounts/edit_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "as a merchant user" do
+  it "I can edit my existing discounts" do
+    merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+    m_user = merchant_1.users.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
+    discount_1 = merchant_1.discounts.create!(percent: 5, quantity_required: 10)
+    discount_2 = merchant_1.discounts.create!(percent: 10, quantity_required: 15)
+
+    visit "/login"
+    fill_in :email, with: m_user.email
+    fill_in :password, with: m_user.password
+    click_button "Log In"
+
+    visit "/merchant/discounts"
+
+    within "#discount-row-#{discount_1}"
+      expect(page).to have_link("Edit")
+    end
+
+    within "#discount-row-#{discount_1}"
+      expect(page).to have_link("Edit")
+      click_link "Edit"
+    end
+
+    expect(current_path).to eq("/merchant/discount/#{discount_1.id}/edit")
+  end
+end
