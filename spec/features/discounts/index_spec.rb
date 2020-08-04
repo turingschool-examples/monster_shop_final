fb831235848d9aedcc9c1ca5cdf27eb57ae0a918
twarbelow <taija.warbelow@google.com>
2020-08-03 20:12:11 -0700

    spec to set up index page

---
+++ spec/features/discounts/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "discounts index page" do
+  it "can be gotten to from a link on the merchant dashboard" do
+    @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+    @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
+
+    visit "/merchant"
+
+    expect(page).to have_link("Current Discounts")
+
+    click_link "Current Discounts"
+
+    expect(current_path).to eq("/merchant/discounts")
+  end
+end
