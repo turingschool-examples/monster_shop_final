1d276e0b5dee313a321769364adf116d714a4583
Eric Hale <eric_hale@comcast.net>
2020-08-04 16:36:00 -0600

    Change welome route

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Cart do
       @m_user = @megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       @discount1 = @m_user.merchant.discounts.create(percentage: 5, item_amount: 5, description: 'Five percent off of five items or more!')
       @discount2 = @m_user.merchant.discounts.create(percentage: 20, item_amount: 10, description: 'Fifteen percent off of ten items or more!')
-      @discount3 = @m_user.merchant.discounts.create(percentage: 15, item_amount: 9, description: 'Fifteen percent off of ten items or more!')
+      @discount3 = @m_user.merchant.discounts.create(percentage: 15, item_amount: 9, description: 'Fifteen percent off of nine items or more!')
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 2 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
