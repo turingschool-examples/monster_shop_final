56d4249643358ed2ba3e4ee17f3002910ab809d7
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 20:27:15 -0400

    Create discounts#index route

--- spec/features/merchant/discount_index_spec.rb
+++ spec/features/merchant/discount_index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Merchant Discount Index' do
   describe 'As a Merchant employee' do
     before :each do
       @megan_shop = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @employee = User.create!(name: 'Gaby Mendez', address: '1422 NE 20th Ave.', city: 'Gainesville', state: 'FL', zip: 32609, role: 1, email: 'employee@hotmail.com', password: 'employee', merchant_id: megan.id)
+      @employee = User.create!(name: 'Gaby Mendez', address: '1422 NE 20th Ave.', city: 'Gainesville', state: 'FL', zip: 32609, role: 1, email: 'employee@hotmail.com', password: 'employee', merchant_id: @megan_shop.id)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@employee)
     end
