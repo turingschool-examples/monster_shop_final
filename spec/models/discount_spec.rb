c29a73e66c0c614ef7bcb4d295b67bb1328f6cec
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-19 11:16:14 -0600

    Add a model spec for Discount

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+    it {should belong_to :merchant}
+    it {should have_many :order_discounts}
+    it {should have_many(:orders).through(:order_discounts)}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+  end
+
+  describe 'Class Methods' do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @discount_1 = @merchant_1.discounts.create!(name: "20% Off")
+      @discount_2 = @merchant_1.discounts.create!(name: "50% Off")
+      @discount_3 = @merchant_1.discounts.create!(name: "75% Off")
+      @user = User.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @order_1 = @user.orders.create!
+      @order_2 = @user.orders.create!
+      @order_3 = @user.orders.create!
+      @order_1.order_discounts.create!(discount: @discount_1)
+      @order_2.order_discounts.create!(discount: @discount_2)
+      @order_3.order_discounts.create!(discount: @discount_3)
+    end
+
+    it '.active_discounts' do
+      expect(Discount.active_discounts).to eq(@discount_1)
+    end
+
+
+  end
+end
