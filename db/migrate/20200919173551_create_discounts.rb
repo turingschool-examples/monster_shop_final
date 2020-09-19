984a6eeca6338ddf68228ba5e47438a292f49daf
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-19 11:41:09 -0600

    Add Discounts migration

---
+++ db/migrate/20200919173551_create_discounts.rb
@@ -0,0 +1,10 @@
+class CreateDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :discounts do |t|
+      t.string :name
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
