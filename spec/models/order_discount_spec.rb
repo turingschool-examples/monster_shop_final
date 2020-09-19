915e699f12b1df4ad19df28781d6575792752b30
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-19 16:32:00 -0600

    Update OrderDiscount model test

--- spec/models/order_discount_spec.rb
+++ spec/models/order_discount_spec.rb
@@ -5,19 +5,4 @@ RSpec.describe OrderDiscount do
     it {should belong_to :order}
     it {should belong_to :discount}
   end
-
-  describe 'instance methods' do
-    before :each do
-      @morgan = Merchant.create!(name: 'Morgans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @discount_1 = @merchant_1.discounts.create!(name: "20% Off")
-      @discount_2 = @merchant_1.discounts.create!(name: "50% Off")
-      @discount_3 = @merchant_1.discounts.create!(name: "75% Off")
-      @user = User.create!(name: 'Morgan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'morgan@example.com', password: 'securepassword')
-      @order_1 = @user.orders.create!
-      @order_2 = @user.orders.create!
-      @order_discount_1 = @order_1.order_discounts.create!(discount: @discount_1)
-      @order_discount_2 = @order_2.order_discounts.create!(discount: @discount_2)
-    end
-  end
 end
