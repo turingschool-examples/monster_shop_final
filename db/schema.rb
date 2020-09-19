b75abb02689d497001ad87548f55f75f2db3e463
Judith Pillado <judith.pillado@gmail.com>
2020-09-19 18:55:20 -0500

    Add nyan-cat-formatter gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190724161751) do
+ActiveRecord::Schema.define(version: 2019_07_24_161751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
