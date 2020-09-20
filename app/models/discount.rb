12fb3abc5442c602ab87d05786b71c5779423269
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-20 11:30:52 -0600

    Update Discount model with new column validations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,7 +3,7 @@ class Discount < ApplicationRecord
   has_many :order_discounts
   has_many :orders, through: :order_discounts
 
-  validates_presence_of :name
+  validates_presence_of :percent, :min_items
 
   def self.active_discounts
     where(active: true)
