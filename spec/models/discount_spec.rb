65b08400d1adcff9a2a49909c7498e859af22a86
Judith Pillado <judith.pillado@gmail.com>
2020-09-20 15:38:32 -0500

    Fix discount percentage message

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Discount do
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_numericality_of(:item_amount).is_greater_than(0)}
-    it {should validate_numericality_of(:discount_percentage)
+    it {should validate_inclusion_of(:discount_percentage)
         .in_range(1..99)
         .with_message("Discount percentage must be 1 - 99")}
   end
