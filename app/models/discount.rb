2e628895b848c81d15926139c3777ba4e7b7a8cd
Judith Pillado <judith.pillado@gmail.com>
2020-09-20 15:26:12 -0500

    Create Discount model - validations and relationships

---
+++ app/models/discount.rb
@@ -0,0 +1,9 @@
+class Discount < ApplicationRecord
+
+  belongs_to :merchant
+
+  validates_presence_of :name
+  validates_numericality_of :item_amount, greater_than: 0
+  validates_inclusion_of :discount_percentage, in: 1..99, message: "Discount percentage must be 1-99"
+
+end
