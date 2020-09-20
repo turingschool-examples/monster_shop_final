0de00e1cf653fd6ea4f511fa38cbd66e7f605232
Corey Carter <crc2231@gmail.com>
2020-09-20 16:36:26 -0600

    Add faker methods to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'faker'
 
 megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+10.times do
+    description = Faker::Movies::HarryPotter.quote
+    name = Faker::Games::Pokemon.unique.name
+    price = Faker::Number.number(digits: 3)
+    image = Faker::Fillmurray.image
+    inventory = Faker::Number.number(digits: 3)
+
+    megan.items.create!(name: name, description: description, price: price, image: image, active: true, inventory: inventory)
+end
+
+10.times do
+    name = Faker::Games::Pokemon.unique.name
+    description = Faker::Movies::HarryPotter.quote
+    price = Faker::Number.number(digits: 3)
+    image = Faker::Fillmurray.image
+    inventory = Faker::Number.number(digits: 3)
+
+    brian.items.create!(name: name, description: description, price: price, image: image, active: true, inventory: inventory)
+end
