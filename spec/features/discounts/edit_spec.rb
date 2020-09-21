48ef6a5f3c68b05dbec2d22c688403ee7452e3f5
Corey Carter <crc2231@gmail.com>
2020-09-21 17:26:08 -0600

    Remove allow_any_instance_of due to database issues

--- spec/features/discounts/edit_spec.rb
+++ spec/features/discounts/edit_spec.rb
@@ -6,12 +6,18 @@ RSpec.describe 'New discount form' do
             @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
             @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
           
-            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+            # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
         end
 
         it 'complete an edit on a discount' do
             discount_1 = @merchant_1.discounts.create(threshold_quantity: 10, discount_percentage: 5)
             discount_2 = @merchant_1.discounts.create(threshold_quantity: 30, discount_percentage: 15)
+
+            visit "/login"
+            
+            fill_in :email, with: @m_user.email
+            fill_in :password, with: @m_user.password
+            click_button "Log In"
 
             visit '/merchant/'
 
@@ -24,15 +30,15 @@ RSpec.describe 'New discount form' do
             expect(find_field('Threshold quantity').value).to eq "10"
             expect(find_field('Discount percentage').value).to eq '5.0'
 
-            fill_in "Threshold quantity", with: 20
-            fill_in "Discount percentage", with: 25
+            new_tq = 20
+            fill_in "Threshold quantity", with: new_tq
             click_button "Update Discount"
             expect(current_path).to eq("/merchant")
             expect(page).to have_content("Discount Updated!")
 
             within "#discount-#{discount_1.id}" do 
-                # expect(page).to have_content("20")
-                # expect(page).to have_content("15")
+                expect(page).to have_content("#{new_tq}")
+                expect(page).to have_content("5.0")
                 expect(page).to have_button("Delete Discount")
                 expect(page).to have_button("Update Discount")
             end
@@ -48,6 +54,12 @@ RSpec.describe 'New discount form' do
         it 'Will not allow me to leave a field empty' do
             discount_1 = @merchant_1.discounts.create(threshold_quantity: 10, discount_percentage: 5)
             discount_2 = @merchant_1.discounts.create(threshold_quantity: 30, discount_percentage: 15)
+            
+            visit "/login"
+            
+            fill_in :email, with: @m_user.email
+            fill_in :password, with: @m_user.password
+            click_button "Log In"
             
             visit '/merchant'
     
