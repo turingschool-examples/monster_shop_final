461755d5e8859f8fffed447e17c40c3fdf5d81ef
Judith Pillado <judith.pillado@gmail.com>
2020-09-20 19:25:03 -0500

    Create action with private method

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,4 +4,21 @@ class Merchant::DiscountsController < Merchant::BaseController
     @discounts = current_user.merchant.discounts
   end
 
+  def new
+  end
+
+  def create
+    @merchant = current_user.merchant
+    @discount = @merchant.discounts.new(discount_params)
+    @discount.save
+    flash[:success] = "New discount created!"
+    redirect_to "/merchant/discounts"
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :item_amount, :discount_percentage)
+  end
+
 end
