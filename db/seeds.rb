f28c2c4d52f8a10409f581574f8c5f9be3098102
Judith Pillado <judith.pillado@gmail.com>
2020-09-22 15:33:44 -0500

    Add discounts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Merchant.destroy_all
-User.destroy_all
-Item.destroy_all
+OrderItem.destroy_all
 Order.destroy_all
-# ItemOrder.destroy_all
+User.destroy_all
+Discount.destroy_all
+Merchant.destroy_all
+Item.destroy_all
+Review.destroy_all
 
 # Merchants:
 megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
@@ -26,9 +28,14 @@ jim = dunder_mifflin.users.create!(name: "James Duncan Halpert", address: "13831
 michaelscott = User.create(name: "Michael Scott", address: "126 Kellum Court", city: "Scranton", state: "PA", zip: 18510, email: "michaelscarn@email.com", password: "holly", password_confirmation: "holly", role: 2)
 
 # Items:
-megan.items.create!(name: 'Orange', description: "Made with sweet oranges and lemon.", price: 10, image: 'https://www.culinaryhill.com/wp-content/uploads/2019/06/culinary-hill-orange-marmalade-square-1.jpg', active: true, inventory: 5 )
-megan.items.create!(name: 'Strawberry', description: "Naturally paleo and vegan.", price: 7, image: 'https://www.texanerin.com/content/uploads/2012/06/how-to-make-strawberry-jam-2.jpg', active: true, inventory: 10 )
-brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-dunder_mifflin.items.create!(name: 'Paper', description: "Premium copy papter", price: 20, image: 'https://dundermifflinpaper.com/wp-content/uploads/2013/06/20190824_185517.jpg', active: true, inventory: 50 )
-dunder_mifflin.items.create!(name: 'Sweatshirt', description: "Soft and perfect for winter.", price: 20, image: 'https://teeshope.com/wp-content/uploads/2019/11/dunder-mifflin-inc-sweatshirt.jpg', active: true, inventory: 25 )
-dunder_mifflin.items.create!(name: 'Pen', description: "Ball point black ink.", price: 3, image: 'https://dundermifflinpaper.com/wp-content/uploads/2019/09/IMG-0738.jpg', active: true, inventory: 50 )
+orange = megan.items.create!(name: 'Orange', description: "Made with sweet oranges and lemon.", price: 10, image: 'https://www.culinaryhill.com/wp-content/uploads/2019/06/culinary-hill-orange-marmalade-square-1.jpg', active: true, inventory: 5 )
+strawberry = megan.items.create!(name: 'Strawberry', description: "Naturally paleo and vegan.", price: 7, image: 'https://www.texanerin.com/content/uploads/2012/06/how-to-make-strawberry-jam-2.jpg', active: true, inventory: 10 )
+hippo = brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+paper = dunder_mifflin.items.create!(name: 'Paper', description: "Premium copy papter", price: 20, image: 'https://dundermifflinpaper.com/wp-content/uploads/2013/06/20190824_185517.jpg', active: true, inventory: 50 )
+sweatshirt = dunder_mifflin.items.create!(name: 'Sweatshirt', description: "Soft and perfect for winter.", price: 20, image: 'https://teeshope.com/wp-content/uploads/2019/11/dunder-mifflin-inc-sweatshirt.jpg', active: true, inventory: 25 )
+pen = dunder_mifflin.items.create!(name: 'Pen', description: "Ball point black ink.", price: 3, image: 'https://dundermifflinpaper.com/wp-content/uploads/2019/09/IMG-0738.jpg', active: true, inventory: 50 )
+
+# Discounts: 
+discount1 = dunder_mifflin.discounts.create!(name: "50% off 10 or more items!", item_amount: 10, discount_percentage: 50)
+discount2 = dunder_mifflin.discounts.create!(name: "20% off 2 or more items!", item_amount: 2, discount_percentage: 20)
+discount3 = megan.discounts.create!(name: "30% off 5 or more items!", item_amount: 2, discount_percentage: 20)
