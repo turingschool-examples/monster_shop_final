254fc2bb8ef23e0e29731707263a7d1edbb4b8e1
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-06 12:40:27 -0700

    Add test for link to discounts on merchant dashboard

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -62,5 +62,13 @@ RSpec.describe 'Merchant Dashboard' do
 
       expect(current_path).to eq("/merchant/orders/#{@order_2.id}")
     end
+
+    it 'I can see bulk discounts' do
+      visit '/merchant'
+
+      click_link 'Bulk Discounts'
+
+      expect(current_path).to eq("/merchant/discounts")
+    end
   end
 end
