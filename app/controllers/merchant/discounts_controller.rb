e51e636ffe9125e12760abeb155891fe5a779b0d
Will Dunlap <dunlapww@gmail.com>
2020-11-07 12:19:12 -0700

    Add functionality for delete and edit buttons on merchant discounts page

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,7 +1,16 @@
 class Merchant::DiscountsController < ApplicationController
 
   def index
-    
+    @merchant = Merchant.find(current_user[:merchant_id])
+  end
+  
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def destroy
+    Discount.delete(params[:id])
+    redirect_to "/merchant/discounts"
   end
 
 end
