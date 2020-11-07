eb57c6ce157dc01dbc2c6679a3ba5d56547acacb
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-07 12:35:37 -0700

    Add route, view, and controller for new page and create action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -3,4 +3,25 @@ class Merchant::DiscountsController < Merchant::BaseController
     @user = User.find(session[:user_id])
     @merchant = Merchant.find(@user.merchant_id)
   end
+
+  def new;end
+
+  def create
+    @user = User.find(session[:user_id])
+    @merchant = Merchant.find(@user.merchant_id)
+    @discount = @merchant.discounts.create(discount_params)
+    if @discount.save
+      flash[:success] = "Discount #{@discount.id} was added."
+      redirect_to '/merchant/discounts'
+    else
+      flash[:error] = @discount.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:quantity, :amount, :merchant_id)
+  end
 end
