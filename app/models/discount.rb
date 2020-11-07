4681081c81a83b38e8f5cccdc275d651e6a3f5e6
George Soderholm <georgesoderholm@gmail.com>
2020-11-06 21:56:07 -0700

    Add a relationship to items through the merchant

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,6 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  has_many :items, through: :merchant
   validates_presence_of :description
   validates_presence_of :quantity
   validates_presence_of :percent
