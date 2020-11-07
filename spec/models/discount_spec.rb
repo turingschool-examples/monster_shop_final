fa40ab8f69d50a0fc4b5697bc16bb62302fe7b3a
Will Dunlap <dunlapww@gmail.com>
2020-11-07 13:45:31 -0700

    Add validation tests for discount model

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -5,4 +5,11 @@ describe Discount do
     it {should belong_to(:item)}
     it {should belong_to(:merchant)}
   end
+
+  describe 'validations' do
+    it {should validate_presence_of :item_id}
+    it {should validate_presence_of :merchant_id}
+    it {should validate_presence_of :threshold}
+    it {should validate_presence_of :discount}
+  end
 end
