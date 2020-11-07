fc86067ba219135e06c838916b08ab9103e60f47
Will Dunlap <dunlapww@gmail.com>
2020-11-07 13:45:47 -0700

    Add discount model validations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,10 @@
 class Discount < ApplicationRecord
   belongs_to :item
   belongs_to :merchant
+
+  validates_presence_of :item_id
+  validates_presence_of :merchant_id
+  validates_presence_of :threshold, greater_than: 0
+  validates_presence_of :discount, in: 0..1
+
 end
