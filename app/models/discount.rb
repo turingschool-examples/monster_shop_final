a964e239229b66acf6f87b8fdda17d9043ac0714
Will Dunlap <dunlapww@gmail.com>
2020-11-07 19:36:00 -0700

    Add item_discount method to discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -10,4 +10,14 @@ class Discount < ApplicationRecord
   validates_numericality_of :discount, greater_than: 0
   validates_numericality_of :discount, less_than: 1
 
+  def self.item_discount(item,order_quantity)
+    discount = Discount
+                  .where(item_id: item.id, merchant_id: item.merchant.id)
+                  .where("discounts.threshold <= ?",order_quantity)
+                  .order(discount: :desc)
+                  .limit(1).first
+
+    discount.nil? ? nil : discount.discount * order_quantity * item.price
+  end
+
 end
