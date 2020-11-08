11be9bc0e49cc796d4082f30f67604d815610ecc
Curtis Bartell <cbartell980@gmail.com>
2020-11-08 14:54:55 -0700

    Wrap raw SQL in Arel.sql() to stop deprecation warnings

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -22,7 +22,7 @@ class Merchant < ApplicationRecord
     orders.joins('JOIN users ON orders.user_id = users.id')
           .order('city_state')
           .distinct
-          .pluck("CONCAT_WS(', ', users.city, users.state) AS city_state")
+          .pluck(Arel.sql("CONCAT_WS(', ', users.city, users.state) AS city_state"))
   end
 
   def pending_orders
