a948ca67584a849592e05f7b5fc8c1cd3d7b8869
George Soderholm <georgesoderholm@gmail.com>
2020-11-09 15:34:10 -0700

    Add a method from cart_concern to help dry out the code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,8 @@ class ApplicationRecord < ActiveRecord::Base
   def full_address
     "#{self.address}, #{self.city}, #{self.state}, #{self.zip}"
   end
+
+  def find_item(item_id)
+    item = Item.find(item_id)
+  end
 end
