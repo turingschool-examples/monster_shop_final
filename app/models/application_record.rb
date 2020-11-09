3a07046b88a4c69768a4453e67554a48822d570a
George Soderholm <georgesoderholm@gmail.com>
2020-11-09 16:52:40 -0700

    Add helper methods from cart concern

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,4 +8,36 @@ class ApplicationRecord < ActiveRecord::Base
   def find_item(item_id)
     item = Item.find(item_id)
   end
+
+  def find_merchant(item_id)
+    item = Item.find(item_id)
+    merchant = Merchant.find(item.merchant_id)
+  end
+
+  def find_all_discounts(item_id)
+    find_merchant(item_id).discounts.order(:quantity)
+  end
+
+  def percentage(discount)
+    ((100 - discount.percent).to_f / 100)
+  end
+
+  def empty_merchant_discount?(item_id)
+    !find_merchant(item_id).discounts.empty?
+  end
+
+  def all_available_discounts(item, quantity)
+    discounted_totals = {}
+    find_all_discounts(item.id).each do |discount|
+      # binding.pry
+      if quantity >= discount.quantity
+        percentage(discount)
+        new_total = item.price * quantity
+        discount_total = new_total * percentage(discount)
+        discounted_totals[discount.id] = new_total - discount_total
+      end
+      binding.pry
+    end
+    @current_discount = Discount.find(discounted_totals.key(discounted_totals.values.max))
+  end
 end
