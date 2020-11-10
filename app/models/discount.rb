2100837bbd8a8233c4107b2a33ec652b1238839a
George Soderholm <georgesoderholm@gmail.com>
2020-11-10 16:56:06 -0700

    Add enabled_status method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,5 +4,14 @@ class Discount < ApplicationRecord
   validates_presence_of :description
   validates_presence_of :quantity
   validates_presence_of :percent
-  validates_presence_of :enable
+  # validates_inclusion_of :enable, :in => [true, false]
+  validates :enable, inclusion: [true, false]
+
+  def enabled_status
+    if enable?
+      message = 'Enabled'
+    else
+      message = 'Disabled'
+    end 
+  end
 end
