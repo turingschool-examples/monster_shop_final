2f1905f3def4b74f6f7cd1a057843211d4d86ba4
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-10 16:05:46 -0700

    Update discount index to test for all crud actions

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'Merchant Dashboard' do
     before :each do
       @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
-      
+      @discount1 = @merchant_1.discounts.create!(percent_off: 0.10, item_requirement: 5)
+      @discount2 = @merchant_1.discounts.create!(percent_off: 0.20, item_requirement: 10)
       visit root_path
+
       within "nav" do
         click_link "Log In"
       end
@@ -16,15 +18,34 @@ RSpec.describe 'Merchant Dashboard' do
       click_button "Log In"
     end 
 
-    it 'I see a link to add a new discount' do
+    it 'I see a link to manage my discounts' do
       visit "/merchant/#{@merchant_1.id}/discounts/index"
 
       expect(page).to have_link("Create a Bulk Discount")
       click_link("Create a Bulk Discount")
 
-      
       expect(current_path).to eq("/merchant/discounts/new")
-
     end 
+
+    it 'I see each of my discounts with a button to edit that discount' do
+      visit "/merchant/#{@merchant_1.id}/discounts/index"
+    
+      expect(page).to have_content("Get #{@discount1.percent_off}% off of orders of #{@discount1.item_requirement} items or more")
+      expect(page).to have_content("Get #{@discount2.percent_off}% off of orders of #{@discount2.item_requirement} items or more")
+      within "#discount-#{@discount1.id}" do
+        click_button("Edit Discount")
+      end 
+
+      expect(current_path).to eq("/merchant/#{@merchant_1.id}/discounts/#{@discount1.id}/edit")
+    end
+
+    it 'I see each of my discounts with a button to delete that discount' do
+      visit "/merchant/#{@merchant_1.id}/discounts/index"
+      within "#discount-#{@discount1.id}" do
+        click_button("Delete Discount")
+      end 
+      
+      expect(current_path).to eq("/merchant/#{@merchant_1.id}/discounts/index")
+    end
   end 
 end
