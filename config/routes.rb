b1738e247f8b2e0ed55085db3c7824064ddfd03a
George Soderholm <georgesoderholm@gmail.com>
2020-11-10 16:57:31 -0700

    Add unRESTful route to deal with the boolean attribute of status

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,9 @@ Rails.application.routes.draw do
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
     resources :discounts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+    resource :discounts do
+      patch '/:status/:id', action: :update_status
+    end
   end
 
   namespace :admin do
