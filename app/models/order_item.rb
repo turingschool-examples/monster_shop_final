7eee3573c4f2d578bc97908406180d5b832ad4cd
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-10 14:24:18 -0700

    Delete unnecssary  method

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,10 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  def subtotal
-    quantity * price
-  end
-
   def fulfill
     update(fulfilled: true)
     item.update(inventory: item.inventory - quantity)
