3a2572533b4b0646feec3ede4ab49fc089afcb89
James Belta <james.belta@gmail.com>
2020-11-11 11:18:42 -0700

    discounts for brian

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 OrderItem.delete_all
 Review.delete_all
+Discount.delete_all
 Item.delete_all
 Order.delete_all
 User.delete_all
@@ -19,9 +20,10 @@ megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city
 brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
 
 #discounts
-discount_5_percent = megan.discounts.create(name: '5 Percent', percentage: 0.05, limit: 5)
-discount_10_percent = megan.discounts.create(name: '10 Percent', percentage: 0.1, limit: 10)
-
+discount_m5_percent = megan.discounts.create(name: '5 Percent', percentage: 0.05, limit: 5)
+discount_m10_percent = megan.discounts.create(name: '10 Percent', percentage: 0.1, limit: 10)
+discount_b5_percent = brian.discounts.create(name: '5 Percent', percentage: 0.05, limit: 5)
+discount_b10_percent = brian.discounts.create(name: '10 Percent', percentage: 0.1, limit: 10)
 #employees ------------------
 
 m_user = megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
