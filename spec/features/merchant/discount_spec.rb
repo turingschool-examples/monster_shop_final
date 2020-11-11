e926b21f9cd0b173d29e4fe4cfa34d3131e9cd5d
James Belta <james.belta@gmail.com>
2020-11-11 06:48:05 -0700

    discount_spec for merchant

---
+++ spec/features/merchant/discount_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Order Show Page' do
+  describe 'As a Merchant' do
+    before :each do
+      @merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @merchant_2 = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+      @discount_1_5 = @merchant_1.discounts.create(name: '5 Percent', percentage: 0.05, limit: 5)
+      @discount_1_10 = @merchant_1.discounts.create(name: '10 Percent', percentage: 0.1, limit: 10)
+      @ogre = @merchant_1.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 20 )
+      @giant = @merchant_1.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 20 )
+      @hippo = @merchant_2.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 20 )
+      @order_1 = @m_user.orders.create!(status: "pending")
+      @order_2 = @m_user.orders.create!(status: "pending")
+      @order_item_1 = @order_1.order_items.create!(item: @hippo, price: @hippo.price, quantity: 5, fulfilled: false)
+      @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 10, fulfilled: true)
+      @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
+      @order_item_4 = @order_2.order_items.create!(item: @giant, price: @giant.price, quantity: 15, fulfilled: false)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+    end
+    it "has a 5% discount on an order with quantity between 5 - 9 on my orders index, orders show, cart and checkout" do
+      order_item_1_regular_price = (@order_item_1.price * @order_item_1.quantity)
+      order_item_1_discounted_price = 200
+
+      @giant.bulk_discount?
+
+      expect(order_item_1_regular_price).to_not eq(order_item_1_discounted_price)
+      expect(order_item_1_regular_price).to eq(250)
+      expect(order_item_1_discounted_price).to eq(200)
+
+      within "#order-#{@order_1.id}" do
+        expect(page).to have_content("Subtotal: $75")
+      end
+
+      visit "/profile/orders/#{@order_1.id}"
+        within "#order-item-#{@order_item_1.id}"
+    end
+  end
+end
