42dedbf4cc6e5a785f191feafb3f8991785c41d8
Hanna Davis <hanna.e.davis@gmail.com>
2020-11-10 18:10:23 -0700

    WIP add calculate price

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,10 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+  def calculate_price
+    self.subtotal / self.quantity
+  end
+
   def fulfill
     update(fulfilled: true)
     item.update(inventory: item.inventory - quantity)
