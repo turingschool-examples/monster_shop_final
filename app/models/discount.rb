5fa3059e52df10ba5e47a53adcc28978a835954c
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-10 21:51:04 -0700

    Add validation to ensure that numbers are valid

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,4 +2,7 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :percent_off,
                         :item_requirement
+
+  validates_numericality_of :percent_off, greater_than: 0, less_than: 1
+  validates_numericality_of :item_requirement, greater_than: 0
 end
