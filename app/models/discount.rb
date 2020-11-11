df352a3aeb765be3d6698c6cf5a18b51d034f019
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-10 22:12:32 -0700

    Add method to display percentage as whole

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -5,4 +5,9 @@ class Discount < ApplicationRecord
 
   validates_numericality_of :percent_off, greater_than: 0, less_than: 1
   validates_numericality_of :item_requirement, greater_than: 0
+
+  def percentage_display
+    (percent_off * 100).to_i
+  end
+  
 end
