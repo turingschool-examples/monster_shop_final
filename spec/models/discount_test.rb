ae85f52c15d172df8a1d4a967c0603f206ee77f5
Sage Gonzales <sagegonzales15@gmail.com>
2020-11-10 22:21:59 -0700

    Add model test for percentage discplay

--- spec/models/discount_test.rb
+++ spec/models/discount_test.rb
@@ -1,12 +1,21 @@
-require 'test_helper'
+require 'rails_helper'
 
 class DiscountTest < ActiveSupport::TestCase
   describe 'Relationships' do
     it {should belong_to :merchant}
   end 
+
   describe 'Validations' do
     it {should validate_presence_of :percent_off}
     it {should validate_presence_of :item_requirement}
-    it {should validate_presence_of :active}
   end 
+
+  describe 'Instance Methods' do
+    it '.percentage_display' do 
+      merchant_1 = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      discount1 = merchant_1.discounts.create!(percent_off: 0.10, item_requirement: 5)
+      
+      expect(discount1.percentage_display).to eq(10)
+    end
+  end
 end 
