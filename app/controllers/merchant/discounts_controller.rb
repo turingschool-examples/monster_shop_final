d6ebdc78f57747b063cf0d73d3ead28a8949d292
James Belta <james.belta@gmail.com>
2020-11-12 02:53:46 -0700

    update action, var.update! not updating information

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -19,12 +19,27 @@ class Merchant::DiscountsController < Merchant::BaseController
     end
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    merchant = current_user.merchant
+    @discount = merchant.discounts.find(params[:id])
+    if @discount.update!(discount_params)
+      @discount.save
+      redirect_to '/merchant/discounts'
+    else
+      flash[:alert] = discount.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
   private
 
   def discount_params
   params.permit(:name, :percentage, :limit)
   end
-  #resources werent providing a prefix on form_with, using form tag
+  #resources was not providing a prefix on form_with, using form tag (create and edit)
   #def discount_params
   #params.require(:discount).permit(:name, :percentage, :limit)
   #end
