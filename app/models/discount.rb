85b151e2195dcb81b4885d8110c217a3858ab5e7
James Belta <james.belta@gmail.com>
2020-11-12 00:23:07 -0700

    model validations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,11 @@
 class Discount < ApplicationRecord
 belongs_to :merchant
 
+validates_presence_of :name,
+                      :percentage,
+                      :limit
+
+
   def self.discount_rate(amount)
     where("discounts.limit <= ?", amount).order(limit: :desc).first
   end
